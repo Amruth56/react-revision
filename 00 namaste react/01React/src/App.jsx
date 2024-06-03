9ecbfe74485bdbcdd01128882e8d0517297e34d6
Amruth <ammumandappa@gmail.com>
2024-06-03 22:26:17 +0530

    starting off with react project

--- 00 namaste react/01React/src/App.jsx
+++ 00 namaste react/01React/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import './index.css'
 import './App.css'
 
 const title = (
+  <a className="anchor" href='/'>  
   <h1 id ="title" key = "h1">
     Food Villa
   </h1>
+  </a>
 )
 const Title2 = ()=> {
   return <h2 id ="title2" key = "h2">calling a function using curly brackets</h2>
@@ -15,12 +17,19 @@ function App() {
   
 
   return (
-    <>
-    {title}
-    {Title2()}
-    <Title2></Title2>
-      <h1>hello world</h1>
-    </>
+    
+    <div className="header">
+      {title}
+      <div className="nav-items">
+        <ul className ="navbar-items">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart </li>
+        </ul>
+      </div>
+    </div>
+    
   )
 }
 
