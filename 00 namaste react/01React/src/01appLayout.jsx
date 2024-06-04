1970216d5a8620fc238f1dcbb827494bf6cbca80
Amruth <ammumandappa@gmail.com>
2024-06-04 23:09:51 +0530

    .

---
+++ 00 namaste react/01React/src/01appLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function AppLayout()  {
+  return (
+    <div>01appLayout</div>
+  )
+}
+
+export default AppLayout
--- 00 namaste react/01React/src/App.jsx
+++ 00 namaste react/01React/src/App.jsx
@@ -1,38 +1,41 @@
-import { useState } from 'react'
-import './index.css'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
+import "./App.css";
+import A ppLayout from "./01appLayout";
 
 const title = (
-  <a className="anchor" href='/'>  
-  <h1 id ="title" key = "h1">
-    Food Villa
-  </h1>
+  <a className="anchor " href="/">
+    <h1 id="title" key="h1">
+      Food Villa
+    </h1>
   </a>
-)
-const Title2 = ()=> {
-  return <h2 id ="title2" key = "h2">calling a function using curly brackets</h2>
-}
+);
+const Title2 = () => {
+  return (
+    <h2 id="title2" key="h2">
+      calling a function using curly brackets
+    </h2>
+  );
+};
 
 function App() {
-  
-
   return (
-    
-    <div className="header">
-      {title}
-      <div className="nav-items">
-        <ul className ="navbar-items">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart </li>
-        </ul>
+    <>
+      <div className="header">
+        {title}
+        <div className="nav-items">
+          <ul className="navbar-items">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Cart </li>
+          </ul>
+        </div>
+        
       </div>
-    </div>
-    
-  )
+      <AppLayout></AppLayout>
+    </>
+  );
 }
-
-
 
 export default App;
